4943a379660943a18325b8efff92f2fb761f98c9
Tatiana Kalinina <109298612+Kalishka82@users.noreply.github.com>
2023-11-10 22:28:14 +0300

    homework_09 added

---
+++ web_09/MyFirstWebApplication/Models/WeatherForecast.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace MyFirstWebApplication.Models
+{
+	/// <summary>
+	/// Прогноз погоды
+	/// </summary>
+	public class WeatherForecast
+	{
+		/// <summary>
+		/// Дата измерения
+		/// </summary>
+		public DateTime Date { get; set; }
+
+		/// <summary>
+		/// Температура по Цельсию
+		/// </summary>
+		public int TemperatureC { get; set; }
+
+		public int TemperatureF
+		{
+			get { return 32 + (int)(TemperatureC * 5 / 9); }
+		}
+	}
+}
+
